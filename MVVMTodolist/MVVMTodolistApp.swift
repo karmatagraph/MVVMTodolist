d0ad6184ed3de005fd393cf09f37627b8344acb7
karma <karmatashi954@gmail.com>
2022-05-30 09:57:16 +0545

    Initial Commit

---
+++ MVVMTodolist/MVVMTodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMTodolistApp.swift
+//  MVVMTodolist
+//
+//  Created by karma on 5/30/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVMTodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
